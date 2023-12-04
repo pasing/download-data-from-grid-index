28dc593aaf52a115a1f9e57e37c5c7780faecdd6
Pasquale Inglese <pasqualeinglese@gmail.com>
2023-12-04 02:49:00 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='download-data-from-grid-index',
+    version='0.1',
+    packages=['download_data_from_grid_index'],
+    url='https://github.com/pasing/download-data-from-grid-index',
+    license='MIT',
+    author='Pasquale Inglese',
+    author_email='pasqualeinglese@gmail.com',
+    description='demo'
+)
