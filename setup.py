623c68dd256f5cb2ff4d9279d8e35b014fc9e456
Pasquale Inglese <pasqualeinglese@gmail.com>
2023-12-04 03:02:53 +0100

    Update setup settings

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+requirements = [
+    'geopandas==0.14.1',
+    'argparse==1.4.0',
+    'requests==2.31.0'
+]
 
 setup(
     name='download-data-from-grid-index',
     version='0.1',
-    packages=['download_data_from_grid_index'],
+    author="Pasquale Inglese",
+    author_email='pasqualeinglese@gmail.com',
     url='https://github.com/pasing/download-data-from-grid-index',
     license='MIT',
-    author='Pasquale Inglese',
-    author_email='pasqualeinglese@gmail.com',
-    description='demo'
+    description='Download data from grid index',
+    python_requires='>=3.6',
+    install_requires=requirements,
+    packages=find_packages(include=['download_data_from_grid_index'])
 )
